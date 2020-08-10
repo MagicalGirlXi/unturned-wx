811c9fcb33bdc203e19d4abcbe72c043abadf434
magicgirlxi <magicgirlxi@qq.com>
2020-08-10 11:54:45 +0800

    添加搜索物品功能
    添加搜索物品功能

--- pages/test/test.js
+++ pages/test/test.js
@@ -3,12 +3,17 @@ var localData = require('../../resources/json.js')
 
 Page({
   data: {
+    searchName: null
   },
   onLoad: function () {
+    wx.showLoading({
+      title: '加载中',
+    })
     this.setData({
       showJsonList: localData.testJson,
       currentLei: localData.testJson[2]
     })
+    wx.hideLoading()
   },
   onLeiClick: function(e) {
     var index = e.target.dataset.id;
@@ -16,5 +21,31 @@ Page({
     this.setData({
       currentLei: localData.testJson[parseInt(index)]
     })
+  },
+  onSearch: function(e) {
+    let result = {
+      "Name": "搜索结果",
+      "Index": "-1",
+      "Contain": []
+    };
+    console.log(e.detail.value);
+    wx.showLoading({
+      title: '搜索中',
+    })
+    
+    for (let lei in this.data.showJsonList) {
+      for (let item in this.data.showJsonList[lei].Contain)
+      {
+        //console.log(this.data.showJsonList[lei].Contain[item].CnName);
+        if (this.data.showJsonList[lei].Contain[item].CnName.includes(e.detail.value))
+        {
+          result.Contain.push(this.data.showJsonList[lei].Contain[item]);
+        }
+      }
+    }
+    this.setData({
+      currentLei: result
+    })
+    wx.hideLoading()
   }
 })
